6ecf5bbdabe90e51c776c21382926534a4acf960
EC2 Default User <ec2-user@ip-172-31-10-137.us-east-2.compute.internal>
2018-04-25 11:19:35 +0000

    ranking

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -14,10 +14,7 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
-    end
-    
-    # Have関係
-    if params[:type] == "Have"
+    elsif params[:type] == "Have"
       current_user.have(@item)
       flash[:success] = "商品を Have しました。"
     end
